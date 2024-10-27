169d6fc1ac8f3f15bd0b54dca8241411ea8e940d
Sutheera P <64070112@kmitl.ac.th>
2024-10-28 00:10:40 +0700

    Auth responsive UI

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,16 @@
-// import { Bird } from "lucide-react";
 import { type PropsWithChildren } from "react";
-
 
 export default function SignUpSignInLayout({ children }: PropsWithChildren) {
     return (
-        <div className="grid h-screen grid-cols-2 bg-black">
-            <div className="flex h-screen items-center justify-center text-white font-bold text-4xl">
-                LOGO
-                {/* <Bird size={300} strokeWidth={1} color="#fff" /> // Please refer to the Lucide docs for more info on icon styling. This can be any icon that you like. */}
+        <>
+            <div className="flex md:grid md:grid-cols-2 min-h-screen bg-black">
+                <div className="hidden md:flex h-screen items-center justify-center text-white font-bold text-4xl">
+                    LOGO
+                </div>
+                <div className="flex items-center justify-center gap-5 w-full py-3">
+                    {children}
+                </div>
             </div>
-            <div className="ml-8 flex flex-col items-center justify-center gap-5">
-                {children}
-            </div>
-        </div>
+        </>
     );
 }
