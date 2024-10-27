fbeb624af025a0c84bbb0c304f5345f29dbdeb93
Sutheera P <64070112@kmitl.ac.th>
2024-10-27 16:30:10 +0700

    Add clerk

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+// import { Bird } from "lucide-react";
+import { type PropsWithChildren } from "react";
+
+
+export default function SignUpSignInLayout({ children }: PropsWithChildren) {
+    return (
+        <div className="grid h-screen grid-cols-2 bg-black">
+            <div className="flex h-screen items-center justify-center text-white font-bold text-7xl">
+                LOGO
+                {/* <Bird size={300} strokeWidth={1} color="#fff" /> // Please refer to the Lucide docs for more info on icon styling. This can be any icon that you like. */}
+            </div>
+            <div className="ml-8 flex flex-col items-center justify-center gap-5">
+                {children}
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import { HydrateClient } from "~/trpc/server";
+import { SignIn, useUser } from '@clerk/nextjs'
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
-  // void api.post.getLatest.prefetch();
+  // const { user } = useUser()
+  // if (!user) {
+  //   return <SignIn />
+  // }
   return (
     <HydrateClient>
-      <div className="p-4">Homepage</div>
+      <main className="p-4 flex flex-col gap-4">
+        <div>Homepage</div>
+        <Link href={'/post'} className="bg-black px-4 py-1 text-white rounded-md w-fit">Post</Link>
+      </main>
+
+
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
