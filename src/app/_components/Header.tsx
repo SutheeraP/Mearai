331f7a937e73e739a153d767eb82cca1c3569f9f
Sutheera P <64070112@kmitl.ac.th>
2024-10-28 08:21:14 +0700

    Add TimeAgo

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 export default function Header() {
     return (
         <>
             <div className='fixed z-10 tracking-wide w-full flex items-center justify-center text-slate-400 font-medium border-b border-slate-500 bg-gray-800 bg-opacity-80 backdrop-blur-sm'>
-                <div className='max-w-[1200px] flex justify-between w-full px-3 h-12 items-center'>
-                    <div>T3-Learning</div>
+                <div className='h-12 max-w-[1200px] w-full flex justify-between items-center px-3'>
+                    <Link href={'/'}>T3-Learning</Link>
                     <div><UserButton /></div>
                 </div>
             </div>
--- src/app/_components/Tweet.tsx
+++ src/app/_components/Tweet.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { type Tweet } from '@prisma/client'
 import Image from 'next/image';
 
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en)
+
 import { createClerkClient } from '@clerk/backend'
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
@@ -10,46 +14,31 @@ type Props = {
 };
 
 export default async function Tweet({ tweet }: Props) {
-  // const userID = tweet.userId
+  const timeAgo = new TimeAgo('en-US')
   const postUser = await clerkClient.users.getUser(tweet.userId)
-  const options = {
-    // year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return (
-    <div>
-      <section
-        key={tweet.id}
-        className="flex gap-2 px-4 py-3"
-      >
-        <div className="flex items-start justify-center">
-          <Image src={postUser.imageUrl} alt='Profile Image' width={40} height={40} className="rounded-full aspect-square object-cover" />
-        </div>
 
-        <div className='flex flex-col gap-3'>
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center gap-2">
-              <h2 className="font-semibold">{postUser.username}</h2>
-              <p>·</p>
-              <p className="text-slate-500">
-                {tweet.timestamp.toLocaleDateString("en-US")}
-              </p>
-              <p className="text-slate-500">
-                {tweet.timestamp.toLocaleTimeString("en-US")}
-              </p>
-            </div>
-            <div>{tweet.text}</div>
+  return (
+    <section
+      key={tweet.id}
+      className="flex gap-2 px-4 py-3"
+    >
+      <div className="flex items-start justify-center">
+        <Image src={postUser.imageUrl} alt='Profile Image' width={40} height={40} className="rounded-full aspect-square object-cover" />
+      </div>
+
+      <div className='flex flex-col gap-3'>
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <h2 className="font-semibold">{postUser.username}</h2>
+            <p>·</p>
+            <p className="text-slate-500">
+              {timeAgo.format(tweet.timestamp)}
+            </p>
           </div>
-          <div className='text-slate-500 text-xs'>delete</div>
-          {/* <div className="grid grid-cols-4 justify-center">
-            <div className="flex items-center">
-              Like
-              <div>{tweet.likes}</div>
-            </div>
-          </div> */}
+          <div>{tweet.text}</div>
         </div>
-      </section>
-    </div>
+        <div className='text-slate-500 text-xs'>delete</div>
+      </div>
+    </section>
   )
 }
