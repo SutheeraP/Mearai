facc389974ff2bf36145cb1ad7cad4c5e0f03971
Sutheera P <64070112@kmitl.ac.th>
2024-10-28 20:27:04 +0700

    Update favicon

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <>
             <div className='fixed z-10 w-full flex justify-center text-slate-400 font-medium border-b border-slate-500 bg-gray-800 bg-opacity-80 backdrop-blur-sm'>
                 <div className='h-12 max-w-[1200px] w-full flex items-center justify-between px-3'>
-                    <Link href={'/'} className='tracking-wide'>Twitraii</Link>
+                    <Link href={'/'} className='tracking-wide font-medium'>Twitraii</Link>
                     <UserButton />
                 </div>
             </div>
