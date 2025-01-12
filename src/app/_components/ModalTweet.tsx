bfc75ade41b755b78d07465bd5ee54b20836243e
Sutheera P <64070112@kmitl.ac.th>
2025-01-12 22:29:08 +0700

    Modal tweet UI 2/2

--- src/app/_components/ModalTweet.tsx
+++ src/app/_components/ModalTweet.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useUser } from '@clerk/nextjs'
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 interface ModalTweetProps {
@@ -7,21 +7,21 @@ interface ModalTweetProps {
 }
 
 export default function ModalTweet({ onClose }: ModalTweetProps) {
-  const { isLoaded, isSignedIn, user } = useUser()
+  const { isLoaded, isSignedIn, user } = useUser();
   if (!isLoaded || !isSignedIn) {
-    return null
+    return null;
   }
 
   return (
-    <div className="absolute top-0 z-10 h-screen w-screen md:bg-main md:bg-opacity-10 md:backdrop-blur-sm">
-      <div className='md:w-[400px] bg-dark px-4'>
+    <div className="absolute top-0 z-10 h-full w-screen md:bg-main md:bg-opacity-10 md:backdrop-blur-sm">
+      <div className="mx-auto h-full bg-dark px-4 md:mt-12 md:h-fit md:w-[500px] md:rounded-md">
         {/* top */}
         <div className="flex items-baseline justify-between py-6">
           <div onClick={onClose} className="cursor-pointer">
             Cancle
           </div>
           <div className="flex items-baseline gap-4">
-            <div className="text-sm">0 / 240</div>
+            {/* <div className="text-sm">0 / 240</div> */}
             <div
               onClick={onClose}
               className="cursor-pointer rounded-full bg-main px-6 py-1 font-semibold text-dark"
@@ -31,28 +31,26 @@ export default function ModalTweet({ onClose }: ModalTweetProps) {
           </div>
         </div>
         {/* content */}
-        <div>
-          <section className="flex gap-4 px-3 py-3 text-sm md:px-4 md:text-base">
-            <div className="relative aspect-square h-12 w-12">
-              <Image
-                src={user.imageUrl}
-                alt="Profile Image"
-                fill
-                className="rounded-full object-cover"
-              />
-            </div>
+        <section className="flex gap-4 pb-6 text-sm md:text-base">
+          <div className="relative aspect-square h-12 w-12">
+            <Image
+              src={user.imageUrl}
+              alt="Profile Image"
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
 
-            <textarea
-              name="tweetText"
-              placeholder="Me a rai?"
-              // onChange={(e) => {
-              //   setTweetText(e.target.value);
-              // }}
-              // value={tweetText}
-              className="h-[300px] w-full bg-transparent text-[16px] placeholder:text-slate-500 focus:outline-none"
-            ></textarea>
-          </section>
-        </div>
+          <textarea
+            name="tweetText"
+            placeholder="Me a rai?"
+            // onChange={(e) => {
+            //   setTweetText(e.target.value);
+            // }}
+            // value={tweetText}
+            className="h-[300px] w-full bg-transparent text-[16px] placeholder:text-slate-500 focus:outline-none"
+          ></textarea>
+        </section>
       </div>
     </div>
   );
