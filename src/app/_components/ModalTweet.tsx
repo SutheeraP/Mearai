15ae5980637f9e194900c83231084442a827bd86
Sutheera P <64070112@kmitl.ac.th>
2025-01-12 22:11:18 +0700

    Modal tweet UI 1/2

--- src/app/_components/ModalTweet.tsx
+++ src/app/_components/ModalTweet.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useUser } from '@clerk/nextjs'
+import Image from "next/image";
 
-export default function ModalTweet() {
+interface ModalTweetProps {
+  onClose: () => void;
+}
+
+export default function ModalTweet({ onClose }: ModalTweetProps) {
+  const { isLoaded, isSignedIn, user } = useUser()
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
+
   return (
-    <div></div>
-  )
+    <div className="absolute top-0 z-10 h-screen w-screen md:bg-main md:bg-opacity-10 md:backdrop-blur-sm">
+      <div className='md:w-[400px] bg-dark px-4'>
+        {/* top */}
+        <div className="flex items-baseline justify-between py-6">
+          <div onClick={onClose} className="cursor-pointer">
+            Cancle
+          </div>
+          <div className="flex items-baseline gap-4">
+            <div className="text-sm">0 / 240</div>
+            <div
+              onClick={onClose}
+              className="cursor-pointer rounded-full bg-main px-6 py-1 font-semibold text-dark"
+            >
+              Post
+            </div>
+          </div>
+        </div>
+        {/* content */}
+        <div>
+          <section className="flex gap-4 px-3 py-3 text-sm md:px-4 md:text-base">
+            <div className="relative aspect-square h-12 w-12">
+              <Image
+                src={user.imageUrl}
+                alt="Profile Image"
+                fill
+                className="rounded-full object-cover"
+              />
+            </div>
+
+            <textarea
+              name="tweetText"
+              placeholder="Me a rai?"
+              // onChange={(e) => {
+              //   setTweetText(e.target.value);
+              // }}
+              // value={tweetText}
+              className="h-[300px] w-full bg-transparent text-[16px] placeholder:text-slate-500 focus:outline-none"
+            ></textarea>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,24 +1,38 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 import React from "react";
-import CreateTweet from "./CreateTweet";
-import ButtonTweet from "./ButtonTweet";
-import getCurrentUser from "../function/currentUser";
 import HomeIcon from "./svg/HomeIcon";
 import NavItem from "./NavItem";
+import ModalTweet from "./ModalTweet";
 import SearchIcon from "./svg/SearchIcon";
 import ProfileIcon from "./svg/ProfileIcon";
+import { useState } from "react";
+import getCurrentUser from "../function/currentUser";
 
-export default async function Navbar() {
-  const user = await getCurrentUser();
+export default function Navbar() {
+  // const currentUser = getCurrentUser();
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
+      {showModal && (
+        <ModalTweet
+          onClose={() => {
+            setShowModal(!showModal);
+          }}
+        />
+      )}
       {/* md */}
       <div className="hidden flex-col gap-2 px-3 text-sm md:flex md:pr-0 md:text-base">
         <NavItem icon={HomeIcon} label="Home" />
         <NavItem icon={SearchIcon} label="Search" />
         <NavItem icon={ProfileIcon} label="Profile" />
-        <div className="mt-4 w-full cursor-pointer rounded-md bg-slate-100 py-2 text-center font-semibold uppercase text-dark hover:bg-main">
+        <div
+          className="mt-4 w-full cursor-pointer rounded-md bg-slate-100 py-2 text-center font-semibold uppercase text-dark hover:bg-main"
+          onClick={() => {
+            console.log("click pc");
+            setShowModal(!showModal);
+          }}
+        >
           Post
         </div>
         {/* {user && <CreateTweet userId={user.id} />} */}
@@ -28,7 +42,13 @@ export default async function Navbar() {
       <div className="md:hidden">
         <div className="absolute bottom-0 w-full">
           <div className="flex w-full justify-end rounded-md p-4">
-            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-slate-100 text-2xl leading-tight text-gray-800">
+            <div
+              onClick={() => {
+                console.log("click");
+                setShowModal(!showModal);
+              }}
+              className="flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-slate-100 text-2xl leading-tight text-gray-800 hover:bg-main"
+            >
               +
             </div>
           </div>
