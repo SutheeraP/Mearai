c8139290f0dd160231f23093ecc716651877d8ba
Sutheera P <64070112@kmitl.ac.th>
2025-03-25 14:53:30 +0700

    Update UI nav

--- src/app/_components/input/NavItem.tsx
+++ src/app/_components/input/NavItem.tsx
@@ -5,19 +5,21 @@ interface NavItemProps {
   icon: React.FC<{ size?: number; color?: string }>;
   label: string;
   path: string;
+  isSelect: boolean;
 }
 
 export default function NavItem({
   icon: IconComponent,
   label,
   path,
+  isSelect,
 }: NavItemProps) {
   return (
-    <div>
+    <div className={`${isSelect ? "text-main" : ""}`}>
       {/* md */}
       <Link
         href={path}
-        className="hidden w-fit cursor-pointer items-center gap-3 border-slate-100 px-3 py-2 hover:border-l md:flex"
+        className="hidden w-fit cursor-pointer items-center gap-3 rounded-md hover:bg-opacity-10 px-3 py-2 transition-all hover:bg-slate-500 md:flex"
       >
         <IconComponent size={15} />
         <div className="font-medium uppercase tracking-wider">{label}</div>
--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -16,6 +16,17 @@ export default function Navbar() {
     error: userError,
   } = api.tweet.getCurrentUser.useQuery();
   const [showModal, setShowModal] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("home");
+  const tabs = [
+    { label: "Home", path: "/", icon: HomeIcon, key: "home" },
+    { label: "Search", path: "search", icon: SearchIcon, key: "search" },
+    {
+      label: "Profile",
+      path: `/${user?.username}`,
+      icon: ProfileIcon,
+      key: "profile",
+    },
+  ];
 
   return (
     <>
@@ -29,13 +40,42 @@ export default function Navbar() {
       )}
       {/* md */}
       <div className="hidden flex-col gap-2 px-3 text-sm md:flex md:pr-0 md:text-base">
-        <NavItem icon={HomeIcon} label="Home" path="/" />
-        <NavItem icon={SearchIcon} label="Search" path="search" />
-        <NavItem
-          icon={ProfileIcon}
-          label="Profile"
-          path={`/${user?.username}`}
-        />
+        <div
+          onClick={() => {
+            setSelectedTab("home");
+          }}
+        >
+          <NavItem
+            icon={HomeIcon}
+            label="Home"
+            path="/"
+            isSelect={selectedTab == "home"}
+          />
+        </div>
+        <div
+          onClick={() => {
+            setSelectedTab("search");
+          }}
+        >
+          <NavItem
+            icon={SearchIcon}
+            label="Search"
+            path="search"
+            isSelect={selectedTab == "search"}
+          />
+        </div>
+        <div
+          onClick={() => {
+            setSelectedTab("profile");
+          }}
+        >
+          <NavItem
+            icon={ProfileIcon}
+            label="Profile"
+            path={`/${user?.username}`}
+            isSelect={selectedTab == "profile"}
+          />
+        </div>
         <div
           className="mt-4 w-full cursor-pointer rounded-md bg-slate-100 py-2 text-center font-semibold uppercase text-dark hover:bg-main"
           onClick={() => {
@@ -63,13 +103,42 @@ export default function Navbar() {
           </div>
 
           <div className="grid grid-cols-3 place-items-center border-t border-slate-500 bg-dark bg-opacity-80 py-5 backdrop-blur-sm">
-            <NavItem icon={HomeIcon} label="Home" path="/" />
-            <NavItem icon={SearchIcon} label="Search" path="search" />
-            <NavItem
-              icon={ProfileIcon}
-              label="Profile"
-              path={`/${user?.username}`}
-            />
+            <div
+              onClick={() => {
+                setSelectedTab("home");
+              }}
+            >
+              <NavItem
+                icon={HomeIcon}
+                label="Home"
+                path="/"
+                isSelect={selectedTab == "home"}
+              />
+            </div>
+            <div
+              onClick={() => {
+                setSelectedTab("search");
+              }}
+            >
+              <NavItem
+                icon={SearchIcon}
+                label="Search"
+                path="search"
+                isSelect={selectedTab == "search"}
+              />
+            </div>
+            <div
+              onClick={() => {
+                setSelectedTab("profile");
+              }}
+            >
+              <NavItem
+                icon={ProfileIcon}
+                label="Profile"
+                path={`/${user?.username}`}
+                isSelect={selectedTab == "profile"}
+              />
+            </div>
           </div>
         </div>
       </div>
